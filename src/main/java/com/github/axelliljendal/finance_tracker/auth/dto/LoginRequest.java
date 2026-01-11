5c6d31a5255aa2353b9431a8fc45e8a43dc48636
Axel Liljendal <liljendalaxel@gmail.com>
2026-01-11 10:46:14 +0100

    Add: annotations and messages for email and password in LoginRequest.java

--- src/main/java/com/github/axelliljendal/finance_tracker/auth/dto/LoginRequest.java
+++ src/main/java/com/github/axelliljendal/finance_tracker/auth/dto/LoginRequest.java
@@ -1,7 +1,15 @@
 package com.github.axelliljendal.finance_tracker.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+
+    @Email(message = "Please provide a valid email address")
+    @NotBlank(message = "You must provide an email address")
     private String email;
+
+    @NotBlank(message = "You must provide a password")
     private String password;
 
     public LoginRequest() {
